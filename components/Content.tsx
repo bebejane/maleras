b5f32ee5f4cec609a6024a17d6d84c53945eb5ef
Bebe Jane <bjornthief@gmail.com>
2024-02-29 13:08:12 +0100

    nada

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,9 +1,10 @@
-import { StructuredContent } from 'next-dato-utils/components'
+import { StructuredContent, Block } from 'next-dato-utils/components'
 import * as blocks from './blocks'
 
 export type Props = {
   content: any
   className?: string
+  children?: typeof Block | typeof Block[]
 }
 
 

38b50f413162473dc5f95ebdb1bf5c0a5f74e988
Bebe Jane <bjornthief@gmail.com>
2024-02-29 15:14:54 +0100

    about us start

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,7 +6,9 @@ import { apiQuery } from 'next-dato-utils/api';
 import { DraftMode, Block } from 'next-dato-utils/components';
 import { StartDocument } from '@graphql';
 import { notFound } from 'next/navigation';
+import { Image } from 'react-datocms';
 import * as BlockComponets from '@components/blocks';
+import Content from '../../components/Content';
 
 export default async function Start({ params }: LocaleParams) {
 
@@ -25,8 +27,10 @@ export default async function Start({ params }: LocaleParams) {
         {start?.content?.map((block, idx) =>
           <Block key={idx} components={BlockComponets} data={block} />
         )}
-        <section>
-          About start section
+        <section className={s.about}>
+          <h2>{start.aboutHeadline}</h2>
+          <Content content={start.aboutText} />
+          <Image data={start.aboutImage.responsiveImage} className={s.image} />
         </section>
       </article>
       <DraftMode url={draftUrl} tag={start?.id} />

6a2037045c494e92038f2a496f90e32740ee8eaa
konst-teknik <information@konst-teknik.se>
2024-03-01 13:27:41 +0100

    About

--- components/blocks/AboutBlock.tsx
+++ components/blocks/AboutBlock.tsx
@@ -4,16 +4,17 @@ import { Image } from 'react-datocms'
 import Link from 'next/link'
 import VideoPlayer from '@components/VideoPlayer'
 import Content from '../Content'
+import cn from 'classnames'
 
 export type LayoutProps = { data: AboutBlockRecord }
 
 export default function AboutBlock({ data: { headline, text, media } }: LayoutProps) {
 
 	return (
-		<section className={s.aboutBlock}>
+		<section className={cn(s.aboutBlock, "grid")}>
 			<div className={s.text}>
 				<h2>{headline}</h2>
-				<Content content={text} />
+				<Content content={text} className="intro" />
 			</div>
 			{media.responsiveImage ?
 				<figure className={s.image}>

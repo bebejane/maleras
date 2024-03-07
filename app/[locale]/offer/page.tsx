1e9ac6b8d64e4cb8033fce12886b5c8ca1b3fbe0
Bebe Jane <bjornthief@gmail.com>
2024-03-07 16:08:08 +0100

    fix2

--- app/[locale]/offer/page.tsx
+++ app/[locale]/offer/page.tsx
@@ -17,7 +17,7 @@ export default async function Offer({ params }: LocaleParams) {
 
   const { offer, allOfferCategories, draftUrl } = await apiQuery<OfferQuery, OfferQueryVariables>(OfferDocument, {
     variables: { siteId: process.env.NEXT_PUBLIC_SITE_ID, locale: params.locale as SiteLocale },
-    tags: ['offer', 'offer_category']
+    tags: ['offer', 'offer_category', 'offer_item']
   });
 
   if (!offer) return notFound();

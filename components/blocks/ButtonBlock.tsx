f18213f3386bd19675843b84412e832834094360
Bebe Jane <bjornthief@gmail.com>
2024-04-08 11:12:05 +0200

    fix

--- components/blocks/ButtonBlock.tsx
+++ components/blocks/ButtonBlock.tsx
@@ -2,10 +2,10 @@ import s from './ButtonBlock.module.scss'
 
 export type LayoutProps = { data: ButtonBlockRecord }
 
-export default function ButtonBlock({ data: { url, text } }: LayoutProps) {
+export default function ButtonBlock({ data: { url, text, openInNewWindow } }: LayoutProps) {
 
 	return (
-		<a className={s.button} href={url}>
+		<a className={s.button} href={url} target={openInNewWindow ? '_blank' : undefined}>
 			<button className="button nav">
 				{text}
 			</button>

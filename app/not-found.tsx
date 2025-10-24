e8fe39e04450a0e6a5edee4bc9caed07c35c0c5c
Bebe Jane <bjornthief@gmail.com>
2025-10-24 15:42:11 +0200

    not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,16 @@
-import Link from 'next/link'
-import s from './not-found.module.scss'
+import Link from 'next/link';
+import s from './not-found.module.scss';
 
 export default function NotFound() {
-  return (
-    <div className={s.container}>
-      <h2>404 - Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
-  )
+	return (
+		<html>
+			<body>
+				<div className={s.container}>
+					<h2>404 - Not Found</h2>
+					<p>Could not find requested resource</p>
+					<Link href='/'>Return Home</Link>
+				</div>
+			</body>
+		</html>
+	);
 }
